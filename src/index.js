6109bed777dc6090ed42271f98b9cec50ec4bf52
Aisana1999 <aisha.asanbaevna.at@gmail.com>
2022-09-20 14:39:08 +0600

    Update index.js
    updated the JS file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,40 @@
+function showPosition(positon) {
+  let latitude = positon.coords.latitude;
+  let longitude = positon.coords.longitude;
+  let apiKey = "c4e575404a843db58519839e2e8ffc17";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentLocationButton = document.querySelector("#button-location");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+//SWITCHING TEMP FROM C TO F
+
+// function changeToF() {
+//   let temperature = document.querySelector(".heading-temperature");
+//   let tempInF = 17 * 1.8 + 32;
+//   temperature.innerHTML = tempInF;
+// }
+// let tempfarenheit = document.querySelector(".fahrenheit");
+// tempfarenheit.addEventListener("click", changeToF);
+
+// function changeToC() {
+//   let temperature = document.querySelector(".heading-temperature");
+//   let tempInC = 17;
+//   temperature.innerHTML = tempInC;
+// }
+// let tempCelsius = document.querySelector(".celsius");
+// tempCelsius.addEventListener("click", changeToC);
+
 let now = new Date();
+console.log(now);
 let days = [
   "Sunday",
   "Monday",
@@ -9,15 +45,15 @@ let days = [
   "Saturday",
 ];
 let day = days[now.getDay()];
-let hours = currentTime.getHours();
+let hours = now.getHours();
 if (hours < 10) {
   hours = `0${hours}`;
 }
-let minutes = currentTime.getMinutes();
+let minutes = now.getMinutes();
 if (minutes < 10) {
   minutes = `0${minutes}`;
 }
-let date = `${day}, ${hour}:${minutes}`;
+let date = `${day}, ${hours}:${minutes}`;
 let currentDate = document.querySelector("#currentDate");
 currentDate.innerHTML = date;
 
